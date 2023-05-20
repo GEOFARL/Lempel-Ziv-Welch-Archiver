eac6ca1bc45fc3d859ed0e27f9e02ef77fc6ceb2
Maxim <toper.one111@gmail.com>
2023-05-20 23:05:46 +0300

    Compress and decompress filenames

--- src/Archiver.cpp
+++ src/Archiver.cpp
@@ -7,6 +7,16 @@ void Archiver::compress(const std::vector<std::string> &inputFiles, const std::s
   // Write the number of input files to the output file
   unsigned int numFiles = inputFiles.size();
   outFile.write(reinterpret_cast<const char *>(&numFiles), sizeof(numFiles));
+
+  for (const std::string &inputFile : inputFiles)
+  {
+    // Write the length of the file name and the file name itself
+    // to the output file
+    unsigned int filenameLength = inputFile.size();
+    outFile.write(reinterpret_cast<const char *>(&filenameLength), sizeof(filenameLength));
+    outFile.write(inputFile.c_str(), filenameLength);
+  }
+
   outFile.close();
 }
 
@@ -18,5 +28,16 @@ void Archiver::decompress(const std::string &inputFilename)
   unsigned int numFiles;
   infile.read(reinterpret_cast<char *>(&numFiles), sizeof(numFiles));
   std::cout << "Read: " << numFiles << std::endl;
+
+  for (unsigned int i = 0; i < numFiles; ++i)
+  {
+    // Read the length of the file name and the file name itself from
+    // the input file
+    unsigned int filenameLength;
+    infile.read(reinterpret_cast<char *>(&filenameLength), sizeof(filenameLength));
+    std::string filename(filenameLength, ' ');
+    infile.read(&filename[0], filenameLength);
+    std::cout << "Filename " << i + 1 << " : " << filename << std::endl;
+  }
   infile.close();
 }
