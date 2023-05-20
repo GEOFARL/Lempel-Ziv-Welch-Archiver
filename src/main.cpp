67631ea9400def7c2a27fe423d622cc53495b3e0
Maxim <toper.one111@gmail.com>
2023-05-20 13:29:03 +0300

    Exclude the possibility to start the program without args

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+  if (argc < 3)
+  {
+    std::cerr << "Usage: " << argv[0] << " [--compress | --decompress] <output_file> [<input_files>...] " << std::endl;
+    return 1;
+  }
   return 0;
 }
