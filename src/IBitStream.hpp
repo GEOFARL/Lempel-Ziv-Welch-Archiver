8745c42a63ac6d81f149baeed089bf2bd7e31b1d
Maxim <toper.one111@gmail.com>
2023-05-23 17:14:03 +0300

    Provide general inteface for bit stream manipulations

--- src/IBitStream.hpp
+++ src/IBitStream.hpp
@@ -1,5 +1,23 @@
 #pragma once
 
+#include <iostream>
+
 class IBitStream
 {
+protected:
+  struct ByteBuffer
+  {
+    std::size_t numOfBitsInUse;
+    unsigned char data;
+
+    ByteBuffer() : numOfBitsInUse{0}, data{0} {}
+  };
+
+  std::size_t binaryWindowLength;
+  ByteBuffer bitCache;
+
+public:
+  virtual std::size_t getBinaryWindowLength() const = 0;
+  virtual void resetBinaryWindowLength() = 0;
+  virtual void increaseBinaryWindowLength() = 0;
 };
