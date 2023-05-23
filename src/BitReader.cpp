8f3ea545d0de3ede01f873f9d1b53d33bad1d333
Maxim <toper.one111@gmail.com>
2023-05-23 18:12:25 +0300

    Define class and its methods

--- src/BitReader.cpp
+++ src/BitReader.cpp
@@ -1,1 +1,23 @@
 #include "BitReader.hpp"
+
+BitReader::BitReader(const std::string &)
+{
+}
+
+std::size_t BitReader::getBinaryWindowLength() const
+{
+}
+void BitReader::resetBinaryWindowLength()
+{
+}
+void BitReader::increaseBinaryWindowLength()
+{
+}
+
+void BitReader::read(uint32_t)
+{
+}
+
+~BitReader::BitReader()
+{
+}
--- src/BitReader.hpp
+++ src/BitReader.hpp
@@ -4,4 +4,16 @@
 
 class BitReader : IBitStream
 {
+  std::ifstream file;
+
+public:
+  BitReader(const std::string &);
+
+  virtual std::size_t getBinaryWindowLength() const override;
+  virtual void resetBinaryWindowLength() override;
+  virtual void increaseBinaryWindowLength() override;
+
+  void read(uint32_t);
+
+  ~BitReader();
 };
