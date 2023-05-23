e22248c2ce27244f095f1d078388f7bae21fad20
Maxim <toper.one111@gmail.com>
2023-05-23 14:32:52 +0300

    Create a Node struct for binary tree

--- src/EncoderDictionary.hpp
+++ src/EncoderDictionary.hpp
@@ -4,4 +4,23 @@
 
 class EncoderDictionary : IDictionary
 {
+  struct Node
+  {
+    uint32_t previousChar;
+    char currentChar;
+    uint32_t left;
+    uint32_t right;
+
+    explicit Node(char current)
+        : previousChar{IDictionary::NULL},
+          currentChar{current},
+          left{IDictionary::NULL},
+          right{IDictionary::NULL}
+    {
+    }
+  };
+
+public:
+  EncoderDictionary();
+  virtual void reset() override;
 };
--- src/IDictionary.hpp
+++ src/IDictionary.hpp
@@ -1,5 +1,14 @@
 #pragma once
 
+#include <cstdint>
+
 class IDictionary
 {
+protected:
+  // 512 KB
+  static const uint32_t MAX_SIZE = 512 * 1024;
+  static const uint32_t NULL = MAX_SIZE;
+
+  virtual void reset() = 0;
+  virtual ~IDictionary() = default;
 };
