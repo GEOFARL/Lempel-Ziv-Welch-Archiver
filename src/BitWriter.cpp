fddbe98829d777d24709178a36c9d8a3ece3ae83
Maxim <toper.one111@gmail.com>
2023-05-23 17:43:57 +0300

    Implement virtual methods

--- src/BitWriter.cpp
+++ src/BitWriter.cpp
@@ -7,10 +7,15 @@ BitWriter::BitWriter(const std::string &fileName)
 
 std::size_t BitWriter::getBinaryWindowLength() const
 {
+  return binaryWindowLength;
 }
+
 void BitWriter::resetBinaryWindowLength()
 {
+  binaryWindowLength = 9;
 }
+
 void BitWriter::increaseBinaryWindowLength()
 {
+  binaryWindowLength += 1;
 }
