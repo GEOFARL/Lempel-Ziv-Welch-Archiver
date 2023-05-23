78305f4490d483c604b4bf4c54c895a148086571
Maxim <toper.one111@gmail.com>
2023-05-23 17:45:05 +0300

    Add check for the correctness of opening a file

--- src/BitWriter.cpp
+++ src/BitWriter.cpp
@@ -3,6 +3,11 @@
 BitWriter::BitWriter(const std::string &fileName)
     : IBitStream(9), file(fileName, std::ios::binary)
 {
+  if (!file)
+  {
+    // Failed to open the file
+    throw std::runtime_error("Failed to open file: " + fileName);
+  }
 }
 
 std::size_t BitWriter::getBinaryWindowLength() const
