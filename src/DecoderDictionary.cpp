d614e911778241d7dd6d5d15e221835157ce2a2f
Maxim <toper.one111@gmail.com>
2023-05-23 15:52:45 +0300

    Implement ctr and reset method

--- src/DecoderDictionary.cpp
+++ src/DecoderDictionary.cpp
@@ -2,8 +2,17 @@
 
 DecoderDictionary::DecoderDictionary()
 {
+  // It's needed to not reallocate memory many times
+  table.reserve(IDictionary::MAX_SIZE);
+  reset();
 }
 
 void DecoderDictionary::reset()
 {
+  table.clear();
+  // Setting initial table of ASCII characters
+  for (int ch = -128; ch < 128; ++ch)
+  {
+    table.push_back({IDictionary::MAX_SIZE, static_cast<char>(ch)});
+  }
 }
